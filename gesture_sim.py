dac601c6b3231033321167a783161a038e3c91ac
Syamkarni Uppalapati <101443460+syamkarni@users.noreply.github.com>
2024-03-22 17:14:59 +0530

    gestures similarities added

---
+++ gesture_sim.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+def sim(kp1, kp2):
+    if not kp1 or not kp2:
+        return float('inf')
+    dist = 0
+    for p1, p2 in zip(kp1, kp2):
+        dist += np.sqrt((p1[0] - p2[0])**2 + (p1[1] - p2[1])**2 + (p1[2] - p2[2])**2)
+    return dist / len(kp1)
